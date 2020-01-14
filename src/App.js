ba2a07d0448273a10696f3ac343c0001410a5021
mlazzell17@gmail.com <MicheSi>
2020-01-14 13:41:27 -0700

    added axios call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+// style imports
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    Axios
+    .get('https://api.github.com/users/michesi')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        users: [res.data]
+      })
+    })
+    .catch(err => {
+      console.log('There is an error', err)
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>React Github User App</h1>
+        {/* <div className='usercard'>
+          {this.state.users.map(user => {
+            <p>{user.name}</p>
+          })}
+
+        </div> */}
+      </div>
+    );
+  }
 }
 
 export default App;
