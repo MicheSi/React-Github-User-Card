f55d21b060e130b2b669f3e4f49b2de210137878
mlazzell17@gmail.com <MicheSi>
2020-01-14 14:30:03 -0700

    added material ui for styling

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    card: {
+    maxWidth: 345,
+    },
+});
 
 const UserCard = props => {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <img src={props.avatar_url} alt={props.name}/>
-            <h4>Username: {props.login}</h4>
-            <p>Number of Repos: {props.public_repos}</p>
-            <p>Number of Followers: {props.followers}</p>
-        </div>
 
+const classes = useStyles();
+
+    return (
+        <Card className={classes.card}>
+            <CardActionArea>
+            <CardMedia
+            component="img"
+            alt="profile picture"
+            height="300"
+            image={props.avatar_url}
+            title={props.name}
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            {props.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {props.login}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            Number of Repos: {props.public_repos}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            Number of Followers{props.followers}
+            </Typography>
+        </CardContent>
+        </CardActionArea>
+        <CardActions>
+        <Button size="small" color="primary">
+            Share
+        </Button>
+        <Button size="small" color="primary">
+            Learn More
+        </Button>
+        </CardActions>
+    </Card>
     )
 }
 
