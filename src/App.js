a3ba25426df833a3a507da8e7268e5db690f3bf3
mlazzell17@gmail.com <MicheSi>
2020-01-14 15:55:26 -0700

    added roboto mono font

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>React Github User App</h1>
+        <h1>React Github User Card</h1>
         <div className='usercard'>
           {this.state.users.map(user => (
             <UserCard
